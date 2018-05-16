ca52efddc43a91c3039546aa9646ce169e4ff2e0
nvashish123 <nitin.vashi@gmail.com>
2018-05-16 11:31:36 -0400

    central time

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
-    currentTime.setTimezone("America/New_York");
+    currentTime.setTimezone("America/Chicago");
     callback(null, {
         statusCode: '200',
         body: 'Now the time in New York is: ' + currentTime.toString(),
