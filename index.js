40100db03ab1b83633e831ff681711f9a486d74e
nvashish123 <nitin.vashi@gmail.com>
2018-05-16 17:56:26 -0400

    new york

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
-    currentTime.setTimezone("America/Chicago");
+    currentTime.setTimezone("America/New_York");
     callback(null, {
         statusCode: '200',
-        body: 'Now the time in Chicago is: ' + currentTime.toString(),
+        body: 'Now the time in NY is: ' + currentTime.toString(),
     });
 };
